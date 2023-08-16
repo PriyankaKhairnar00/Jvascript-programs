84a015ec1f47149058f712e2565857a2793024a0
Priyanka Khairnar <khairnarpriyanka00@gmail.com>
2023-08-16 18:11:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+
+// var myAge=10+"20";
+// console.log("myAge");   
+
+//console.log(10+"20");//1020
+//console.log(9-"5");// 4   //bug
+//  console.log("java"+"script");//javascript
+//   console.log(""+"");
+//  console.log(""+0);//0
+// console.log("Piu"-"Borse");//Nan -not a number
+// console.log(true+true);//2 
+// -because true represent 1 & false represent 0.
+//console.log(true+false);//1
+// console.log(false+true);//1
+// console.log(false-true);//-1  
+
+// Leaf year challenge
+// var year=2024;
+// if(year%4===0){
+//     if(year%100===0){
+//         if(year%400===0){
+//     console.log("the year "+year+" is a leap year");
+
+//         }else{
+//             console.log("the year "+year+" is not a leap year");
+
+//         }
+
+//     }else{
+//     console.log("the year "+year+" is a leap year");
+
+//     }
+
+// }else{
+//     console.log("the year"+year+"is not a leap year");
+// }
+
+
+// For loop
+
+// table
+for(let num=1;num<=10;num++){
+    // console.log("8"+ "*" + num +"=",num*8);
+    var tableOf=10;
+    console.log(tableOf+"*"+num +"="+ tableOf*num);
+}
+
